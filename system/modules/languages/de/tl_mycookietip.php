f95c629c47a7a1185e8f11e4deb0f94e83869cbe
Thoni <github@thonak.de>
2018-09-20 11:36:59 +0200

    Add files via upload

---
+++ system/modules/languages/de/tl_mycookietip.php
@@ -0,0 +1,55 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Frank Thonak www.thomkit.de
+ * @author     Frank Thonak
+ * @package    mycookietip
+ * @license    LGPL
+ * @filesource
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_mycookietip']['text'] = array('Hinweistext','');
+$GLOBALS['TL_LANG']['tl_mycookietip']['label_start'] = 'Hinweis-Daten';
+
+
+$GLOBALS['TL_LANG']['tl_mycookietip']['name'] = array('Name','Der Name dient nur der Identifizierung in der vorangehenden Liste.');
+$GLOBALS['TL_LANG']['tl_mycookietip']['buttontext'] = array('Button-Text','Geben Sie hier den Text f&uuml;r den Best&auml;tigungsbutton ein.');
+$GLOBALS['TL_LANG']['tl_mycookietip']['runtime'] = array('Laufzeit','Geben Sie an, wieviele tage nach der Best&auml;tigung der Hinweis nicht mehr gezeigt werden soll.');
+$GLOBALS['TL_LANG']['tl_mycookietip']['label_control'] = 'Hinweis-Steuerung';
+$GLOBALS['TL_LANG']['tl_mycookietip']['site_control'] = array('Hinweis nicht auf allen Seiten zeigen','');
+$GLOBALS['TL_LANG']['tl_mycookietip']['layouts'] = array('Seitenlayouts','Bitte w&auml;hlen Sie hier die Seitenlayouts aus, bei denen der Hinweis gezeigt werden soll.');
+$GLOBALS['TL_LANG']['tl_mycookietip']['published'] = array('Hinweis aktivieren','');
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_mycookietip']['edit'] = array('Cookie-Hinweis bearbeiten','Cookie-Hinweis ID %s bearbeiten');
+$GLOBALS['TL_LANG']['tl_mycookietip']['toggle'] = array('Cookie-Hinweis (de)aktivieren','Cookie-Hinweis ID %s (de)aktivieren');
+$GLOBALS['TL_LANG']['tl_mycookietip']['delete'] = array('Cookie-Hinweis l&ouml;schen','Cookie-Hinweis ID %s l&ouml;schen');
+$GLOBALS['TL_LANG']['tl_mycookietip']['new']    = array('Neuer Cookie-Hinweis', 'Neuer Cookie-Hinweis');
+
+?>
